873fd8fed826f3804d9a33db4e6ac3bfb2425807
louuu <louaytrc@gmail.com>
2024-10-21 00:06:21 +0100

    design

--- app/src/main/java/com/example/locationapp/CarList.java
+++ app/src/main/java/com/example/locationapp/CarList.java
@@ -2,6 +2,7 @@ package com.example.locationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
